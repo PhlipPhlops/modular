a82e1340907fe4af8ad1a4e80aadc4201ec27f8e
PK Morgan <morgana@localhost.localdomain>
2021-01-06 20:12:44 -0500

    Initting ModularGrid component for further development

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,12 +1,14 @@
 import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
 import TweetList from './components/TweetList';
+import ModularGrid from './components/ModularGrid';
 import './App.css';
 
 const message = 'Welcome to frontend-react';
 const App = () => (
   <div className="App">
     <h1>{message}</h1>
+    <ModularGrid />
     <TweetList />
   </div>
 );
---
+++ frontend_react/src/components/ModularGrid.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ModularGrid extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Modular</p>
+      </div>
+    )
+  }
+};
